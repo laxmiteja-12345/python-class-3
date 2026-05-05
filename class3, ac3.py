e3a9008a15eb0b5a46f6a36d1ef540c84dfc5ea6
laxmiteja-12345 <laxmitejaadlapalli959@gmail.com>
2026-05-05 15:37:58 +0530

    Add files via upload

---
+++ class3, ac3.py
@@ -0,0 +1,6 @@
+str1="Hello"
+str2="World"
+print('str1+str2=', str1+str2)
+
+text="python is fun"
+print(text.upper())
---
+++ class3,ac2.py
@@ -0,0 +1,24 @@
+age= 15
+print("Age :", age)
+print("Data type of age is:", type(age))
+
+age=str(age)
+print(age)
+print("data type of age is:", type(age))
+
+weight=38.5
+print("Weight:", weight)
+print("Data type of weight is:", type(weight))
+
+print("\n After type casting..")
+
+weight=int(weight)
+print(weight)
+print("Data type of weight is:", type(weight))
+
+d="20"
+print(d)
+print("Data type of d is:", type(d))
+d=int(d)
+print(d)
+print("Date type of d is:", type(d))
